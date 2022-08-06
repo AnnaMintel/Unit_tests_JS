79f038ab9c1580a7e773249e3daef0cab2b180ac
AnnaMintel <annikamintel99@yandex.ru>
2022-08-06 23:11:28 +0300

    feat: created new test

--- src/10/10.test.ts
+++ src/10/10.test.ts
@@ -1,31 +1,49 @@
-import { UserType } from './10';
+import { cutHair, UserType, UserWithLaptopType, moveUser } from './10';
 
-const cutHair = (u: UserType , cutLength: number) => {
-    // никогда ничего не меняем в данных, которые приходят. Обязательно создаем копию и с ней работаем
-    // а затем уже в конце можем переприсвоить новое значение старому. ОДнако функция должна сохраниться чистой
-    const userCopy = { 
-        ...u,
-        hair: u.hair / cutLength
-    }
-    // userCopy.hair = u.hair / cutLength
 
-    return userCopy
-}
 
 test('check type test', () => {
     let user: UserType = {
         name: 'Ivan',
         hair: 70,
         adress: { 
-            title: 'Kazan'
+            city: 'Kazan'
+
         }
     }
 
     const cutUser = cutHair(user , 2)
 
-    user.adress.title = 'Kiev'
+    user.adress.city = 'Kiev' 
 
     expect(user.hair).toBe(70)
     expect(cutUser.hair).toBe(35)
     expect(cutUser.adress).toBe(user.adress)
+})
+
+test('change adress', () => {
+    let user: UserWithLaptopType = {
+        name: 'Ivan',
+        hair: 70,
+        adress: { 
+            city: 'Minsk',
+            house: 12
+        },
+        laptop: {
+            title: 'Asus'
+        }
+    }
+
+    const movedUser = moveUser(user , 'Kiev')
+
+
+    // expect(user.hair).toBe(70)
+    // expect(cutUser.hair).toBe(35)
+    // expect(cutUser.adress).toBe(user.adress)
+
+    expect(user).not.toBe(movedUser)
+    expect(user.adress).not.toBe(movedUser.adress)
+    expect(user.laptop).toBe(movedUser.laptop)
+    expect(movedUser.adress.city).toBe('Kiev')
+
 })
--- src/10/10.ts
+++ src/10/10.ts
@@ -1,5 +1,36 @@
 export type UserType = {
     name: string
     hair: number
-    adress: {title:string}
+    adress: {city:string, house?: number}
+}
+
+export type LaptopType = {
+    title: string
+}
+export type UserWithLaptopType = UserType & {
+    laptop: LaptopType
+}
+
+export const cutHair = (u: UserType , cutLength: number) => {
+    // никогда ничего не меняем в данных, которые приходят. Обязательно создаем копию и с ней работаем
+    // а затем уже в конце можем переприсвоить новое значение старому. ОДнако функция должна сохраниться чистой
+    const userCopy = { 
+        ...u,
+        hair: u.hair / cutLength
+    }
+    // userCopy.hair = u.hair / cutLength
+
+    return userCopy
+}
+
+export const moveUser = (u: UserWithLaptopType, city: string) => {
+    const userCopy = { 
+        ...u,
+        adress : {
+            ...u.adress, 
+            city: city
+        }
+    }
+
+    return userCopy
 }
